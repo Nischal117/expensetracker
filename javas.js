211d4b6a4d310239cccb79df8686bdbae2e7eacb
Nischal <107695989+Nischal117@users.noreply.github.com>
2022-07-12 08:53:45 +0545

    Update javas.js

--- javas.js
+++ javas.js
@@ -1,9 +1,57 @@
 
 
+const incomeText = document.querySelector(".money-plus")
+const expenseText = document.querySelector(".money-minus")
+
+let income = 0
+let expense = 0
+
+
 function myFunction(){
+
 let text = document.querySelector("#text").value
 let amount = document.querySelector("#amount").value
 
+
+
+const ulElement = document.getElementById("list")
+
+   let liElement = document.createElement("li")
+
+liElement.classList.add(
+   amount > 0 ? "plus" : "minus"
+)
+liElement.innerText = text
+
+
+let spanElement = document.createElement("span")
+amount > 0 ? spanElement.innerText = "+Rs" + amount : spanElement.innerText = "-Rs" + amount.slice(1)
+//spanElement.innerText = amount
+
+
+
+if (amount > 0){
+   let profit = income + amount
+
+incomeText.innerText = parseFloat(profit) 
+
+}
+if (amount < 0){
+   let loss = expense + amount
+   
+incomeText.innerText =parseFloat(loss)
+}
+
+
+let btnElement = document.createElement("button")
+btnElement.innerText = "X"
+btnElement.classList.add("dlt-btn")
+
+   
+
+liElement.appendChild(spanElement);
+liElement.appendChild(btnElement);
+ulElement.appendChild(liElement);
 }
 
    
